d3d18d9615ec4d67fe353043243db3a91832ed55
King <167127942+Waweru2024@users.noreply.github.com>
2026-08-03 17:37:48 +0300

    Add users router to server

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import routes from "./routes/index.js";
+import usersRouter from "./routes/users.js";
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/", routes);
+app.use("/users", usersRouter);
 
 const PORT = process.env.PORT || 5000;
 
